6ef7ecf12e0836d7bb3254e81f3696d0334274f8
Terry-rose <2216mayet@gmail.com>
2023-11-14 03:43:08 -0800

    commit

--- printf_d_i.c
+++ printf_d_i.c
@@ -1,31 +1,48 @@
-#include "main.h"
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
+/**
+ * _printf - Custom printf function
+ * @format: Format string with conversion specifiers
+ * Return: Number of characters printed (excluding null byte)
+ */
 int _printf(const char *format, ...)
 {
-int count = 0;
 va_list args;
+
+int count = 0;
+
+const char *ptr;
 
 va_start(args, format);
 
-while (*format != '\0')
+for (ptr = format; *ptr != '\0'; ++ptr)
 {
-if (*format == '%' && (*(format + 1) == 'd' || *(format + 1) == 'i'))
+if (*ptr == '%' && (*(ptr + 1) == 'd' || *(ptr + 1) == 'i'))
 {
-int value = va_arg(args, int);
-printf("%d", value);
-count++;
-format += 2;
+int num = va_arg(args, int);
+char buffer[12];
+
+sprintf(buffer, "%d", num);
+
+count += write(1, buffer, strlen(buffer));
+
+++ptr;
+}
+else if (*ptr == '%' && *(ptr + 1) == '%')
+{
+count += write(1, "%", 1);
+++ptr;
 }
 else
 {
-putchar(*format);
-count++;
-format++;
+count += write(1, ptr, 1);
 }
 }
 
 va_end(args);
 return (count);
 }
+
