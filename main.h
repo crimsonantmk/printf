cd611ca213e4982a4febc2302e20e4aae9a25f90
crimsonantmk <crimsonant69@gmail.com>
2023-11-16 14:30:03 -0800

    1.0

--- main.h
+++ main.h
@@ -2,7 +2,11 @@
 #define MAIN_H
 
 #include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
 
+int binary(int num);
+int octal(int num);
 int _printf(const char *format, ...);
 
 #endif /* MAIN_H */
